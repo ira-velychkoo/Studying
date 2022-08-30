72b2cb294f2dc7a820f801020682a1f1249b2e3b
ira-velychkoo <111498895+ira-velychkoo@users.noreply.github.com>
2022-08-30 11:37:55 +0300

    Added weather description functionality

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ function showWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  let description = document.querySelector ("#weatherDespription");
+  description.innerHTML = response.data.weather[0].description;
 }
 
 function citySearch(event) {
